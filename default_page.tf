c656aded550cbffec3bf078f8d56eeecb3d364a0
kocharthur <kochaarthur@gmail.com>
2022-01-18 00:46:35 -0500

    Updated files

---
+++ default_page.tf
@@ -0,0 +1,31 @@
+data "aws_ami" "amazon" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+}
+
+
+resource "aws_instance" "default_page" {
+  ami                         = data.aws_ami.amazon.id
+  instance_type               = "t2.micro"
+  key_name                    = "DevOps14"
+  subnet_id                   = aws_subnet.prdx-public1-subnet.id
+  vpc_security_group_ids      = [aws_security_group.prdx-proje-sg.id]
+  associate_public_ip_address = true
+  user_data                   = <<EOF
+    #!/bin/bash
+    sudo yum install -y httpd
+    cd /var/www/html
+    sudo wget https://s3-terraform-bucket-patra.s3.us-east-2.amazonaws.com/default/index-default.html
+    sudo wget https://s3-terraform-bucket-patra.s3.us-east-2.amazonaws.com/default/2015-dodge-charger-srt-hellcat-front-left-angle-625x417-c.jpeg
+    sudo mv index-default.html index.html
+    sudo systemctl start httpd
+    sudo systemctl enable httpd --now
+EOF
+  tags = {
+    Name = "prdnx-default"
+  }
+}
